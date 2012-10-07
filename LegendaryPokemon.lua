9765d8e89a8a8eff44f24101af98101136465f80
tekkub :bear:  <tekkub@gmail.com>
2012-10-07 06:04:55 -0600

    Simplify frame creation

--- LegendaryPokemon.lua
+++ LegendaryPokemon.lua
@@ -1,47 +1,33 @@
 
--- Create frames for the Glow for Enemy/Player, plus 1 more for the Tooltip
-local frames = {}
-for i=1,7 do
-	frames[i] = CreateFrame("Frame", "PetBattleQuality_Glow"..i, UIParent)
-	frames[i].Glow = frames[i]:CreateTexture("Glow"..i, "ARTWORK")
-	frames[i].Glow:SetTexture("Interface\\Buttons\\UI-ActionButton-Border", false)
-	frames[i].Glow:SetBlendMode("ADD")
-end
+local textures = setmetatable({}, {
+	__index = function(t,i)
+		local f = CreateFrame("Frame", nil, i)
+		f:SetWidth(i.Icon:GetWidth() * 1.7)
+		f:SetHeight(i.Icon:GetHeight() * 1.7)
+		f:SetPoint("CENTER", i.Icon)
+
+		local tex = f:CreateTexture(nil, "ARTWORK")
+		tex:SetAllPoints(f)
+		tex:SetDrawLayer("ARTWORK", 1)
+		tex:SetTexture("Interface\\Buttons\\UI-ActionButton-Border", false)
+		tex:SetBlendMode("ADD")
+
+		t[i] = tex
+		return tex
+	end
+})
 
 
 hooksecurefunc("PetBattleUnitFrame_UpdateDisplay", function(self)
-	-- There must be a petOwner and a petIndex
 	if not (self.petOwner and self.petIndex) then return end
 
-	-- Set which Glow frame this will use (Enemy Frames are +3 / Tooltip is 7)
-	local is_tooltip = self:GetName() == "PetBattlePrimaryUnitTooltip"
-	local is_enemy = self.petOwner == LE_BATTLE_PET_ENEMY and 3 or 0
-	local i = is_tooltip and 7 or (self.petIndex + is_enemy)
-	local f = frames[i]
-
-	-- Position the Glow, tricky to set it over Icon, but under Border
-	f:SetParent(self)
-	f:ClearAllPoints()
-	f:SetWidth(self.Icon:GetWidth() * 1.7)
-	f:SetHeight(self.Icon:GetHeight() * 1.7)
-	f:SetPoint("CENTER", self.Icon, "CENTER", 0, 0)
-
-	-- Set the texture for the Glow
-	f.Glow:SetParent(self)
-	f.Glow:ClearAllPoints()
-	f.Glow:SetAllPoints(f)
-	f.Glow:SetDrawLayer("ARTWORK", 1)
-
-	-- Set the color for the Glow
+	local is_tooltip = self == PetBattlePrimaryUnitTooltip
+	local name = C_PetBattles.GetName(self.petOwner, self.petIndex)
 	local qual = C_PetBattles.GetBreedQuality(self.petOwner, self.petIndex) - 1
 	local r, g, b, hex = GetItemQualityColor(qual)
-	f.Glow:SetVertexColor(r, g, b)
 
-	-- Color the Name with the Quality color
-	if self.Name then
-		local sPetName = C_PetBattles.GetName(self.petOwner, self.petIndex)
-		if (sPetName) then self.Name:SetText("|c"..hex..sPetName.."|r") end
-	end
+	textures[self]:SetVertexColor(r, g, b)
+	if self.Name and name then self.Name:SetText("|c"..hex..name.."|r") end
 
 	-- Color the non-active Health Bars with the Quality color
 	if self.ActualHealthBar and not is_tooltip and self.petIndex ~= 1 then
